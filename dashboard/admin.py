2ed789f83be8202d03a41a417c86e8ce43c2ad54
Erik Helmers <erik.helmers@outlook.fr>
2024-09-11 00:10:27 +0200

    db: add base models

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 
+from .models import Sample, Algo, Hash
+
+admin.site.register(Algo)
+admin.site.register(Hash)
+admin.site.register(Sample)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 
+
+class Sample(models.Model):
+
+
+    sid = models.CharField(max_length=512, unique=True)
+    name = models.CharField(max_length=512)
+    path = models.CharField(max_length=4096, unique=True)
+    signature = models.CharField(max_length=1024)
+
+    def __str__(self):
+        return self.sid
+
+
+
+class Algo(models.Model):
+    name = models.CharField(max_length=512)
+    sid = models.CharField(max_length=512, unique=True)
+    path = models.CharField(max_length=4096, unique=True)
+
+    def __str__(self):
+        return self.sid
+
+
+class Hash(models.Model):
+
+    value = models.TextField()
+    algo = models.ForeignKey(Algo, on_delete=models.CASCADE)
+    sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
+
+
+    class Meta:
+        unique_together = [["algo", "sample"]]
+
+
+    def __str__(self):
+        return f"{self.algo.sid}/{self.sample.sid}"
