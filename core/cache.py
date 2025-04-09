7670f2d68488a42334ac53f5d8a2e0bca8df1ac7
Erik Helmers <erik.helmers@outlook.fr>
2025-04-09 16:19:10 +0200

    core: update caching mechanism

--- core/cache.py
+++ core/cache.py
@@ -1,7 +1,6 @@
-from .core import Benchmark
 from pathlib import Path
 from sqlalchemy import String, Float, create_engine, Column, tuple_
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker, Session
+from sqlalchemy.orm import DeclarativeBase, sessionmaker, Session
 from sqlalchemy.types import TypeDecorator
 
 
@@ -13,7 +12,7 @@ class PathColumnType(TypeDecorator):
     cache_ok = True
 
     def process_bind_param(self, value, dialect):
-        return str(value.as_posix()) if value is not None else None
+        return str(value) if value is not None else None
     def process_result_value(self, value, dialect):
         return Path(value)
 
@@ -26,50 +25,47 @@ class DbResult(Base):
     alt = Column(PathColumnType, primary_key=True)
     dist = Column(Float, primary_key=False)
 
-    def __repr__(self):
-        return (f"Result(runner='{self.runner}', ref='{self.ref}', "
-                f"alt='{self.alt}', dist={self.dist})")
-
     @staticmethod
-    def from_result(runner, res):
-        ref, alt, dist = res
+    def from_result(result):
         return DbResult(
-            runner=runner,
-            ref=ref,
-            alt=alt,
-            dist=dist)
-
-def session_from_path(path: Path):
-    url = f"sqlite:///{str(path)}"
-    engine = create_engine(url, echo=False)
-    Base.metadata.create_all(engine)
-    return sessionmaker(bind=engine)()
+            runner=result.runner.id,
+            ref=result.entry.ref,
+            alt=result.entry.alt,
+            dist=result.distance)
 
 class Cache:
 
-    def __init__(self, session : Session = None):
-        if session is None: session = sessionmaker(bind=create_engine("sqlite://", echo=True))()
-        self.session = session
+    def __init__(self, path):
+        url = f"sqlite:///{str(path)}"
+        engine = create_engine(url, echo=False)
+        Base.metadata.create_all(engine)
+        self.session = sessionmaker(bind=engine)()
 
-    def save_results(self, runner:str, results:list[tuple[Path, Path, float]]):
+    def save_results(self, results):
         self.session.bulk_save_objects([
-            DbResult.from_result(runner, res)
+            DbResult.from_result(res)
             for res in results
         ])
         self.session.commit()
 
-    def update_results(self, runner:str, results:list[tuple[Path, Path, float]]):
-        # NOTE: this is slow
+    def update_results(self, results):
+        # this is rather slow
         for res in results:
-            self.session.merge(DbResult.from_result(runner, res))
+            self.session.merge(DbResult.from_result(res))
         self.session.commit()
 
     def get_results(self, runner, pairs):
-        rows = (self.session.query(DbResult.ref, DbResult.alt, DbResult.dist)
+        rows = (self.session.query(DbResult.runner, DbResult.ref, DbResult.alt, DbResult.dist)
             .filter(DbResult.runner == runner)
             .filter(tuple_(DbResult.ref, DbResult.alt).in_(pairs))
             .all())
 
-        output = [(ref, alt, dist) for ref, alt, dist in rows]
-        remaining = list(set(pairs) - set((ref, alt) for (ref, alt, _) in output))
-        return output, remaining
+        return [(runner, ref, alt, dist) for runner, ref, alt, dist in rows]
+
+    def populate_results(self, results):
+        pairs = [(res.entry.ref, res.entry.alt) for res in results]
+        runner = results[0].runner.id
+        result_idx = { (res.entry.ref, res.entry.alt): res for res in results }
+
+        for _, ref, alt, dist in self.get_results(runner, pairs):
+            result_idx[(ref, alt)].distance = dist
