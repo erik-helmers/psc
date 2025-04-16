558fdc4e2c6c0a0fcf420c6b5eebf02cfcdce64a
Erik Helmers <erik.helmers@outlook.fr>
2025-04-16 18:52:46 +0200

    core/analysis: add rates in ConfusionMatrix

--- core/analysis.py
+++ core/analysis.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 from functools import reduce
-from collections import defaultdict
 import pandas as pd
 import numpy as np
 from dataclasses import dataclass
@@ -22,6 +21,19 @@ class ConfusionMatrix:
     @property
     def tn(self): return self.raw[1, 1]
 
+    @property
+    def tpr(self): return self.tp / (self.tp + self.fn)
+
+    @property
+    def fnr(self): return self.fn / (self.tp + self.fn)
+
+    @property
+    def tnr(self): return self.tn / (self.fp + self.tn)
+
+    @property
+    def fpr(self): return self.fp / (self.fp + self.tn)
+
+
     def f_score(self) -> float:
         """Compute the F-score."""
         precision = self.precision()
@@ -37,7 +49,7 @@ class ConfusionMatrix:
         return (self.fp + self.fn) / (self.tp + self.tn + self.fp + self.fn)
 
     def detection_rate(self) -> float:
-        """Compute the detection rate."""
+        """Compute the detection rate (recall)"""
         if self.tp + self.fn == 0: return 0.0
         return self.tp / (self.tp + self.fn)
 
@@ -52,6 +64,10 @@ class ConfusionMatrix:
             'fp': self.fp,
             'fn': self.fn,
             'tn': self.tn,
+            'tpr': self.tpr,
+            'fpr': self.fpr,
+            'fnr': self.fnr,
+            'tnr': self.tnr,
             'precision': self.precision(),
             'error_rate': self.error_rate(),
             'detection_rate': self.detection_rate(),
@@ -63,11 +79,10 @@ class ConfusionMatrix:
 def confusion_matrix(df_pos, df_neg, threshold):
     out = np.zeros((2, 2), dtype=np.int64)
 
-    print(df_pos.loc[df_pos['distance'] <= threshold].size)
     out[0, 0] = df_pos.loc[df_pos['distance'] <= threshold].size
-    out[0, 1] = df_pos.loc[df_pos['distance'] > threshold].size
-    out[1, 0] = df_neg.loc[df_neg['distance'] <= threshold].size
-    out[1, 1] = df_neg.loc[df_neg['distance'] > threshold].size
+    out[0, 1] = df_neg.loc[df_neg['distance'] <= threshold].size
+    out[1, 0] = df_pos.loc[df_pos['distance'] >  threshold].size
+    out[1, 1] = df_neg.loc[df_neg['distance'] >  threshold].size
 
     return ConfusionMatrix(out)
 
@@ -84,7 +99,6 @@ def confusion_df(df_pos, df_neg, thresholds = None):
     for threshold in thresholds:
         cm = confusion_matrix(df_pos, df_neg, threshold)
         dcm = cm.asdict() | { "threshold": threshold }
-        print(dcm)
         data.append(dcm)
 
     return pd.DataFrame(data)
