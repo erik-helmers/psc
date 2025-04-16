c1d60ae7b60b74b5accadda5abe6f57da20fd256
Erik Helmers <erik.helmers@outlook.fr>
2025-04-16 15:55:35 +0200

    core/analysis: confusion matrix and related

--- core/analysis.py
+++ core/analysis.py
@@ -1,7 +1,94 @@
 from pathlib import Path
 from functools import reduce
 from collections import defaultdict
+import pandas as pd
 import numpy as np
+from dataclasses import dataclass
+
+
+@dataclass
+class ConfusionMatrix:
+    raw: np.ndarray
+
+    @property
+    def tp(self): return self.raw[0, 0]
+
+    @property
+    def fp(self): return self.raw[0, 1]
+
+    @property
+    def fn(self): return self.raw[1, 0]
+
+    @property
+    def tn(self): return self.raw[1, 1]
+
+    def f_score(self) -> float:
+        """Compute the F-score."""
+        precision = self.precision()
+        recall = self.detection_rate()
+        return 2 * (precision * recall) / (precision + recall)
+
+    def precision(self) -> float:
+        """Compute the precision."""
+        return self.tp / (self.tp + self.fp)
+
+    def error_rate(self) -> float:
+        """Compute the error rate."""
+        return (self.fp + self.fn) / (self.tp + self.tn + self.fp + self.fn)
+
+    def detection_rate(self) -> float:
+        """Compute the detection rate."""
+        if self.tp + self.fn == 0: return 0.0
+        return self.tp / (self.tp + self.fn)
+
+    def false_alarm_rate(self) -> float:
+        """Compute the false-alarm rate."""
+        return self.fp / (self.fp + self.tn)
+
+    def asdict(self):
+        """Convert to a dictionary."""
+        return {
+            'tp': self.tp,
+            'fp': self.fp,
+            'fn': self.fn,
+            'tn': self.tn,
+            'precision': self.precision(),
+            'error_rate': self.error_rate(),
+            'detection_rate': self.detection_rate(),
+            'false_alarm_rate': self.false_alarm_rate(),
+            'f_score': self.f_score()
+        }
+
+
+def confusion_matrix(df_pos, df_neg, threshold):
+    out = np.zeros((2, 2), dtype=np.int64)
+
+    print(df_pos.loc[df_pos['distance'] <= threshold].size)
+    out[0, 0] = df_pos.loc[df_pos['distance'] <= threshold].size
+    out[0, 1] = df_pos.loc[df_pos['distance'] > threshold].size
+    out[1, 0] = df_neg.loc[df_neg['distance'] <= threshold].size
+    out[1, 1] = df_neg.loc[df_neg['distance'] > threshold].size
+
+    return ConfusionMatrix(out)
+
+
+def confusion_df(df_pos, df_neg, thresholds = None):
+
+    if thresholds is None:
+        val_min = min(df_pos['distance'].min(), df_neg['distance'].min())
+        val_max = max(df_pos['distance'].max(), df_neg['distance'].max())
+        thresholds = np.linspace(val_min, val_max, 100)
+
+    data = []
+
+    for threshold in thresholds:
+        cm = confusion_matrix(df_pos, df_neg, threshold)
+        dcm = cm.asdict() | { "threshold": threshold }
+        print(dcm)
+        data.append(dcm)
+
+    return pd.DataFrame(data)
+
 
 def pretty(df):
     df['ref'] = df['ref'].apply(lambda x: Path(x).name)
